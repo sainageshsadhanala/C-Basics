739b87627f2b134864aa3b9d856d4e47b229d85f
sainageshsadhanala <92150726+sainageshsadhanala@users.noreply.github.com>
2021-10-08 15:17:16 +0530

    Add files via upload

---
+++ increment_decrement_operator.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+	printf("enter number\n");
+	scanf("%d",&a);
+
+	printf("a++=%d\nb=%d\n",a++,b=a);
+	printf("++a=%d\nb=%d\n",++a,b=a);
+	printf("a--=%d\nb=%d\n",a--,b=a);
+		printf("--a=%d\nb=%d\n",--a,b=a);
+}
