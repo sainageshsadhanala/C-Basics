2b0d98a041f08ac23a175e15dc5315a6957d4f31
sainageshsadhanala <92150726+sainageshsadhanala@users.noreply.github.com>
2021-10-08 15:33:29 +0530

    Add files via upload

---
+++ arithmatic_operators.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+	printf("enter 2 numbers\n");
+	scanf("%d%d",&a,&b);
+
+	printf("sum=%d\n",a+b);
+	printf("difference=%d\n",a-b);
+	printf("product=%d\n",a*b);
+	printf("quotient=%d\n",a/b);
+	printf("remainder=%d\n",a%b);
+}
