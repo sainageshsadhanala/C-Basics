04c03d69f9424d8e4030094c81304efb878fdc36
sainageshsadhanala <92150726+sainageshsadhanala@users.noreply.github.com>
2021-10-09 13:48:59 +0530

    Add files via upload

---
+++ PERSONAL_DETAILS.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+printf("NAME:S.G.SAI NAGESH\n");
+printf("father's name:S.ADINARAYANA\n");
+printf("mother's name:S.MANGADEVI\n");
+printf("BROTHER's name:S.P.V.DURGA RAO");
+}
