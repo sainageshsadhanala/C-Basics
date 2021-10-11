339eef6d9326264a4fd18737eab5d99e6be6e7af
sainageshsadhanala <92150726+sainageshsadhanala@users.noreply.github.com>
2021-10-11 15:22:19 +0530

    Add files via upload

---
+++ total_and_percentage_of_students_if_else_ladder.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+int main()
+{
+    int tel,eng,mat,sci,soc;
+    float obt,per;
+	printf("enter marks in telugu");
+	scanf("%d",&tel);
+	printf("enter marks in english");
+	scanf("%d",&eng);
+	printf("enter marks in maths");
+	scanf("%d",&mat);
+	printf("enter marks in science");
+    scanf("%d",&sci);
+    printf("enter marks in social");
+    scanf("%d",&soc);
+    obt=tel+eng+mat+sci+soc;
+    printf("total marks=%f\n",obt);
+    per=(obt/500)*100;
+	//max marks in each subject are assumed to be 100
+    printf("percentage=%.2f\n",per);
+    if(per>=90)
+      {
+    	printf("grade A");
+	  }
+	else if(per>=80 && per<90)
+      {
+    	printf("grade B");
+	  }
+	else if(per>=70 && per<80)
+      {
+    	printf("grade C");
+	  }
+	else if(per>=60 && per<70)
+      {
+    	printf("grade D");
+	  }
+	else if(per>=50 && per<60)
+      {
+    	printf("grade E");
+	  }
+	else
+	  {
+		printf("fail");
+		
+	  }
+}
