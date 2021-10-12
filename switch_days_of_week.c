59ffd36d1f66271ce34099f1fbec9fef8f0b03e9
sainageshsadhanala <92150726+sainageshsadhanala@users.noreply.github.com>
2021-10-12 15:03:55 +0530

    Add files via upload

---
+++ switch_days_of_week.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("enter the number between 1 and 7\n");
+	scanf("%d",&a);
+	switch(a)
+	{
+		case 1:printf("sunday");
+		       break;
+		case 2:printf("monday");
+		       break;
+		case 3:printf("tuesday");
+		       break;
+	    case 4:printf("wednesday");
+		       break;
+		case 5:printf("thursday");
+		       break;
+		case 6:printf("friday");
+		       break;
+		case 7:printf("saturday");
+		       break;
+		default:printf(" invalid input");
+	}
+	
+}
