13cf8a555655cbd5f4a9641b9feaa3367dd97d9e
sainageshsadhanala <92150726+sainageshsadhanala@users.noreply.github.com>
2021-10-18 15:00:08 +0530

    Add files via upload

---
+++ for_sum_of_even_numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int i,s=0,n;
+	printf("enter a number");
+	scanf("%d",&n);
+	for(i=0;i<=n;i++)
+	{
+		if(i%2 == 0)
+		{
+			s=s+i;
+		}
+	}
+	printf("sum of odd numbers=%d",s);
+}
